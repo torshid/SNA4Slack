526b92a8d0a4276ff2bc04c39f27d4879c98bce6
yazidabdulsalam <32582441+yazidabdulsalam@users.noreply.github.com>
2017-11-25 17:25:30 +0300

    Updated server.py
    server modified

--- server.py
+++ server.py
@@ -1,53 +1,44 @@
-from flask import Flask, render_template, request
-from werkzeug.exceptions import NotFound, Forbidden
-
-import jinja
+from flask import *
+import socket
 from GraphInfo import do_it
-from config import *
+import os, uuid
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+
+@app.route("/")
+def hello():
+    #json_data = do_it(api_key='',threshold = 0,sna_metric = 'Degree')
+    #print(json_data)
+    return render_template('home.html')
 
 @app.route('/show', methods = ['POST', 'GET'])
 def show():
     key = request.form['slack-key']
-    threshold = request.form['threshold']
+    #threshold = request.form['threshold']
     sna_metric = request.form['sna-metric']
     
-    print(do_it(key,threshold,sna_metric))
+    json_data = do_it(key,0,sna_metric)
+    print(json_data)
     # need to save the data
     # graph uses cached data have to make new file and send that filename to html
-    # so we can se it in alchemy config
-    return render_template('graph_sample.html')
+    # so we can see it in alchemy config
+    save_path = './static/graph_data'
+    file_name = str(uuid.uuid4()) + ".json"
+    realFileName = os.path.join(save_path, file_name)
+    file = open(realFileName,'w+')
+    file.write(json_data)
+    file.close()
 
-# jinja-python functions
-@app.context_processor
-def processor():
-    functions = {}
-    for function in jinja.__dict__.values():
-        if callable(function):
-            functions[function.__name__] = function
-    return functions
+    allFiles = [f for f in os.listdir(save_path) if os.path.isfile(os.path.join(save_path, f))]
+    for f in allFiles:
+        if f != file_name:
+            try:
+                os.remove(os.path.join(save_path, f))
+            except OSError as e:
+                print(e)
 
-# dynamically load all entities + register blueprints
-for name in os.listdir("entities"):
-    if name.endswith(".py"):
-        module = name[:-3]
-        globals()[module] = __import__('entities.' + module, fromlist = ['page'])
-        app.register_blueprint(getattr(globals()[module], 'page'))
+    return render_template('graph.html',file_name=file_name)
 
-
-@app.errorhandler(NotFound)
-def error(e):
-    return render_template('errors/' + str(e.code) + '.html'), e.code
-
-
-@app.errorhandler(Forbidden)
-def error(e):
-    return render_template('errors/' + str(e.code) + '.html'), e.code
-
-
-
-
-if __name__ == '__main__':
-    app.secret_key = flaskkey
-    app.run(host = '0.0.0.0', port = port, debug = debug)
+if __name__ == "__main__":
+    app.run(host = "127.0.0.1", port = 8080, debug= True)
